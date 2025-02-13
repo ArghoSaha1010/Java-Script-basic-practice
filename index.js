2c6c7b1e62c302a5f35a5bb366f5e9bd9603ef7b
ArghoSaha1010 <arghosaha1010@gmail.com>
2025-02-13 23:41:04 +0600

    added some codes on js

---
+++ index.js
@@ -0,0 +1,3 @@
+const number = [3234,234,234,23,23,42,34,2,34,23,42,34,234,234,234];
+console.log(number);
+console.log(number[number.length - 1]);
---
+++ length.js
@@ -0,0 +1,2 @@
+const number = [23,34,35,45,2,35,24,23,3];
+console.log(number.length);
